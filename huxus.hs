5ebf7e859443e9d00ea83303d0cb792f6129a8f0
Hannes Gräuler <hgraeule@uos.de>
2012-12-31 17:22:54 +0100

    Implement scale, rotate, translate wrappers

--- huxus.hs
+++ huxus.hs
@@ -12,11 +12,10 @@ intToGLfloat :: Float -> GLfloat
 intToGLfloat x = realToFrac x
 
 myPoints :: GLfloat -> [(GLfloat,GLfloat,GLfloat)]
-myPoints t = map (\k -> (sin(2*pi*k/32)/2.0+cos(t+k/15.0)/4.0,cos(k/32*pi*k/32)/2.0+cos(t/4.0+k/15.0)/2.0,0.0)) [1..32]
-        where tx = t / 2.0
+myPoints t = map (\k -> (sin(2*pi*k/32)/2.0+cos(tx+k/15.0)/4.0,cos(k/32*pi*k/32)/2.0+cos(tx/4.0+k/15.0)/2.0,0.0)) [1..32]
+        where tx = t / 1000.0
 
 timeInSeconds [] = do x <- time; return $ Number $ floor $ realToFrac x
-
 timeInMilliSeconds [] = do x <- time; return $ Number $ floor $ 1000 * realToFrac x
 
 makeLine :: [LispVal] -> IO LispVal
@@ -24,10 +23,27 @@ makeLine [Number t] = do
     renderPrimitive Lines $ mapM_ (\(x, y, z)->vertex$Vertex3 x y z) (myPoints (intToGLfloat $ realToFrac t))
     return (Bool True)
 
-makeColor :: [LispVal] -> IO LispVal
-makeColor [Number r, Number g, Number b] = do
-    putStrLn $ show r
+doColor :: [LispVal] -> IO LispVal
+doColor [Number r, Number g, Number b] = do
     color $ Color3 (togl r) (togl g) (togl b)
+    return (Bool True)
+        where togl x = ((intToGLfloat . realToFrac) x) / 255.0
+
+doTranslate :: [LispVal] -> IO LispVal
+doTranslate [Number r, Number g, Number b] = do
+    translate $ Vector3 (togl r) (togl g) (togl b)
+    return (Bool True)
+        where togl x = ((intToGLfloat . realToFrac) x) / 255.0
+
+doScale :: [LispVal] -> IO LispVal
+doScale [Number x, Number y, Number z] = do
+    scale (togl x) (togl y) (togl z)
+    return (Bool True)
+        where togl x = ((intToGLfloat . realToFrac) x) / 255.0
+
+doRotate :: [LispVal] -> IO LispVal
+doRotate [Number a, Number x, Number y, Number z] = do
+    rotate (togl a) $ Vector3 (togl x) (togl y) (togl z)
     return (Bool True)
         where togl x = ((intToGLfloat . realToFrac) x) / 255.0
 
@@ -59,7 +75,7 @@ makeCube [] = do
         vertex $ Vertex3 (-w) (-w) (-w)
         vertex $ Vertex3 (-w) w (-w)
     return (Bool True)
-    where w = 0.1 :: GLfloat
+    where w = 1.0 :: GLfloat
 
 makeThrowErrorFunc :: ([LispVal] -> IO LispVal) -> [LispVal] -> IOThrowsError LispVal
 makeThrowErrorFunc f obj = do
@@ -69,16 +85,21 @@ makeThrowErrorFunc f obj = do
 -- TODO "secs" should be a variable, not a funcion?! or at least cached
 fluxPrimitives = [ ("make-line", f makeLine),
                    ("make-cube", f makeCube),
-                   ("color", f makeColor),
+
+                   ("color", f doColor),
+                   ("scale", f doScale),
+                   ("translate", f doTranslate),
+                   ("rotate", f doRotate),
+
                    ("secs", f timeInSeconds),
                    ("msecs", f timeInMilliSeconds)
                  ]
                     where f = IOFunc . makeThrowErrorFunc
 
-
 main = do 
   (progname, [filename]) <- getArgsAndInitialize
   source <- readFile filename
+  putStrLn source
   createWindow progname
   displayCallback $= display source
   idleCallback $= Just idle
@@ -88,7 +109,7 @@ display source = do
   clear [ColorBuffer]
   -- TODO: the following line should not be done every frame
   env <- primitiveBindings >>= flip bindVars fluxPrimitives
-  evalString env $ source ++ "(every-frame)"
+  preservingMatrix $ evalString env $ source ++ "(every-frame)"
   flush
 
 idle = do
--- initial.scm
+++ initial.scm
@@ -1,1 +1,1 @@
-(define every-frame (cons (color (mod (msecs) 255) 200 100) (make-cube) (color 100 0 (mod (msecs) 255)) (make-line (msecs))))
+(define every-frame (cons (color 100 0 (mod (msecs) 255)) (make-line (msecs)) (scale (mod (/ (msecs) 20) 64) (mod (/ (msecs) 10) 64) 255) (make-cube) (rotate (* (msecs) 25) 0 0 255) (make-cube)  (color (mod (msecs) 255) 20 100) (translate 0 (* (mod (/ (msecs) 100) 64) 20) 0) (make-cube)))
