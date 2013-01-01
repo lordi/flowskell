df558abf65cddcbd104e41b67156fe0987d5a7c4
Hannes Gräuler <hgraeule@uos.de>
2013-01-01 19:23:50 +0100

    multi-line version

--- initial.scm
+++ initial.scm
@@ -1,1 +1,11 @@
-(define every-frame (cons (color 100 0 (mod (msecs) 255)) (make-line (msecs)) (scale (mod (/ (msecs) 20) 64) (mod (/ (msecs) 10) 64) 255) (make-cube) (rotate (* (msecs) 25) 0 0 255) (make-cube)  (color (mod (msecs) 255) 20 100) (translate 0 (* (mod (/ (msecs) 100) 64) 20) 0) (make-cube)))
+(define every-frame
+  (cons
+    (color 100 0 (mod (msecs) 255))
+    (make-line (msecs))
+    (scale (mod (/ (msecs) 20) 64) (mod (/ (msecs) 10) 64) 255)
+    (make-cube)
+    (rotate (* (msecs) 25) 0 0 255)
+    (make-cube)
+    (color (mod (msecs) 255) 20 100)
+    (translate 0 (* (mod (/ (msecs) 100) 64) 20) 0)
+    (make-cube)))
