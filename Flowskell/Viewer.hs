f6b04158d639b792891b797664216d724894e42f
Hannes Gräuler <graeuler@geoplex.de>
2013-01-04 10:03:46 +0100

    Cleanup

--- Flowskell/Viewer.hs
+++ Flowskell/Viewer.hs
@@ -13,34 +13,27 @@ import Flowskell.Lib.GL (glIOPrimitives)
 import Flowskell.Lib.Random (randomIOPrimitives)
 import Flowskell.Lib.Time (timeIOPrimitives)
 
-makeThrowErrorFunc :: ([LispVal] -> IO LispVal) -> [LispVal] -> IOThrowsError LispVal
-makeThrowErrorFunc f obj = do
-    x <- liftIO $ f obj
-    return x
-
-otherPrimitives :: [ ((String, String), LispVal) ]
-otherPrimitives = map (\(n, f) -> (("v", n), IOFunc $ makeThrowErrorFunc f)) $
-                timeIOPrimitives ++ glIOPrimitives ++ randomIOPrimitives
+primitives :: [ ((String, String), LispVal) ]
+primitives = map (\(n, f) -> (("v", n), IOFunc $ makeThrowErrorFunc f)) other
+                where makeThrowErrorFunc f obj = liftIO $ f obj
+                      other = timeIOPrimitives ++ glIOPrimitives ++ randomIOPrimitives
 
 main = let light0 = Light 0 in do
   (progname, [filename]) <- getArgsAndInitialize
   source <- readFile filename
   initialDisplayMode $= [DoubleBuffered, RGBMode, WithDepthBuffer]
   createWindow progname
-
   ambient (Light 0) $= Color4 0.2 0.2 0.2 1
   diffuse (Light 0) $= Color4 1 1 1 0.6
   position (Light 0) $= Vertex4  0 0 3 0
   lightModelAmbient $= Color4 0.2 0.2 0.2 1
   lightModelLocalViewer $= Disabled
-
   frontFace $= CW
   lighting $= Enabled
   light (Light 0) $= Enabled
   autoNormal $= Enabled
   normalize $= Enabled
   depthFunc $= Just Less
-
   matrixMode $= Projection
   loadIdentity
   let near = 0
@@ -49,15 +42,8 @@ main = let light0 = Light 0 in do
       top = 3
   frustum (-right) right (-top) top near far
   matrixMode $= Modelview 0
-  matrixMode $= Projection
 
-  -- perspective 40.0 1.0 1.0 10.0
-
-  matrixMode $= Modelview 0
-
-  --lookAt (Vertex3 0.0 0.0 5.0) (Vertex3 0.0 0.0 0.0) (Vector3 0.0 1.0 0.0)
-
-  env <- primitiveBindings >>= flip extendEnv otherPrimitives
+  env <- primitiveBindings >>= flip extendEnv primitives
 
   let exprs = extractValue $ readExprList $ source in do
     forM exprs $ runIOThrows . liftM show . evalLisp env
@@ -66,7 +52,7 @@ main = let light0 = Light 0 in do
   idleCallback $= Just idle
   mainLoop
 
-display env = do 
+display env = do
   clear [ColorBuffer, DepthBuffer]
   preservingMatrix $ evalString env "(every-frame)" >>= putStr
   swapBuffers
