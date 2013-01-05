f519a37ba5b7e286345e81fea47959d31d838936
Hannes Gräuler <hgraeule@uos.de>
2013-01-05 16:08:18 +0100

    shape function (not functioning yet)

--- Flowskell/Viewer.hs
+++ Flowskell/Viewer.hs
@@ -35,14 +35,6 @@ main = let light0 = Light 0 in do
   autoNormal $= Enabled
   normalize $= Enabled
   depthFunc $= Just Less
-  matrixMode $= Projection
-  loadIdentity
-  let near = 0
-      far = 100
-      right = 3
-      top = 3
-  frustum (-right) right (-top) top near far
-  matrixMode $= Modelview 0
 
   env <- primitiveBindings >>= flip extendEnv primitives
 
@@ -51,7 +43,36 @@ main = let light0 = Light 0 in do
     forM exprs $ putStrLn . show
   displayCallback $= display env
   idleCallback $= Just idle
+  reshapeCallback $= Just reshape
   mainLoop
+
+{-
+reshape screenSize@(Size w h) = do
+  viewport $= ((Position 0 0), screenSize)
+  matrixMode $= Projection 
+  loadIdentity
+  let near   = 0.1
+      far    = 140
+      fov    = 130
+      ang    = (fov*pi)/(360)
+      top    = near / ( cos(ang) / sin(ang) )
+      aspect = fromIntegral(w)/fromIntegral(h)
+      right  = top*aspect
+  frustum (-right) right (-top) top near far
+  matrixMode $= Modelview 0
+-}
+
+reshape s@(Size w h) = let aspect = (fromIntegral w) / (fromIntegral h) in do
+  viewport $= ((Position 0 0), s)
+  matrixMode $= Projection
+  loadIdentity
+  let near = 0.0
+      far = 100
+      top = 1.0
+      right = 1.0 * aspect
+  putStrLn $ show (top, right)
+  frustum (-right) right (-top) top near far
+  matrixMode $= Modelview 0
 
 display env = do
   clear [ColorBuffer, DepthBuffer]
