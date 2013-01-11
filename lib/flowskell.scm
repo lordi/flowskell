034b88f3533ddf47511ee5377823b27fa8911c35
Hannes Gräuler <hgraeule@uos.de>
2013-01-11 18:37:43 +0100

    Solar system example

--- lib/flowskell.scm
+++ lib/flowskell.scm
@@ -3,9 +3,13 @@
 (define y-axis (vector 0.0 1.0 0.0))
 (define z-axis (vector 0.0 0.0 1.0))
 
+(define % modulo)
+
 (define white (vector 1.0 1.0 1.0))
 (define gray (vector 0.5 0.5 0.5))
 (define black (vector 0.0 0.0 0.0))
 (define red (vector 1.0 0.0 0.0))
-(define blue (vector 0.0 1.0 0.0))
-(define green (vector 0.0 0.0 1.0))
+(define green (vector 0.0 1.0 0.0))
+(define blue (vector 0.0 0.0 1.0))
+(define yellow (vector 1.0 1.0 0.0))
+
