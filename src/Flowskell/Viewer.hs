0b43da52a5b7dc4e65c33d033e9e6daa2a3c8086
Hannes Gräuler <hgraeule@uos.de>
2013-01-27 11:24:44 +0100

    texture test...

--- src/Flowskell/Viewer.hs
+++ src/Flowskell/Viewer.hs
@@ -1,17 +1,58 @@
 module Flowskell.Viewer where
+import Control.Monad (when)
+import Data.Maybe (listToMaybe, isJust, fromJust)
 import Data.IORef
 import Graphics.Rendering.OpenGL hiding (Bool, Float)
 import Graphics.Rendering.OpenGL.GLU (perspective)
 import Graphics.Rendering.GLU.Raw
+import Graphics.Rendering.OpenGL.GL.FramebufferObjects
 import Graphics.UI.GLUT hiding (Bool, Float)
 import Flowskell.Interpreter (initSchemeEnv, evalFrame, evalLisp')
 import Language.Scheme.Types (LispVal (Atom, String))
 
+import Foreign ( withArray )
+
+imageSize :: TextureSize2D
+imageSize = TextureSize2D 64 64
+
+withImage :: (PixelData (Color3 GLubyte) -> IO ()) -> IO ()
+withImage act =
+   withArray [ Color3 (s (sin ti)) (s (cos (2 * tj))) (s (cos (ti + tj))) |
+               i <- [ 0 .. fromIntegral w - 1 ],
+               let ti = 2 * pi * i / fromIntegral w,
+               j <- [ 0 .. fromIntegral h - 1 ],
+               let tj = 2 * pi * j / fromIntegral h ] $
+   act . PixelData RGB UnsignedByte
+   where (TextureSize2D w h) = imageSize
+         s :: Double -> GLubyte
+         s x = truncate (127 * (1 + x))
+
 viewer = let light0 = Light 0 in do
   (progname, [filename]) <- getArgsAndInitialize
 
+
+  -- Framebuffer test
+  fbo <- genObjectNames 1
+  let fbName = head fbo
+  bindFramebuffer Framebuffer $= fbName
+
+  exts <- get glExtensions
+  putStrLn $ show exts
+  {- texName' <- if "GL_EXT_texture_object" `elem` exts
+                 then fmap listToMaybe $ genObjectNames 1
+                 else return Nothing -}
+  texName <- fmap listToMaybe $ genObjectNames 1
+  when (isJust texName) $ textureBinding Texture2D $= texName
+  putStrLn $ show texName
+
+  framebufferTexture2D Framebuffer (ColorAttachment 0) Nothing (fromJust texName) 0
+
+  -- /Framebuffer test
+
   initialDisplayMode $= [DoubleBuffered, RGBMode, WithDepthBuffer]
   createWindow progname
+
+
   ambient light0 $= Color4 0.2 0.2 0.2 1
   diffuse light0 $= Color4 1 1 1 0.6
   position light0 $= Vertex4 0 0 3 0
@@ -25,6 +66,21 @@ viewer = let light0 = Light 0 in do
   autoNormal $= Enabled
   normalize $= Enabled
   depthFunc $= Just Less
+  --cullFace $= Just Front
+
+  --let fb = (FramebufferObject 1) :: FramebufferObject
+
+
+  -- Texture test
+  textureFunction $= Decal
+  textureWrapMode Texture2D S $= (Repeated, Repeat)
+  textureWrapMode Texture2D T $= (Repeated, Repeat)
+  textureFilter Texture2D $= ((Nearest, Nothing), Nearest)
+  --withImage $ texImage2D Nothing NoProxy 0 RGB' imageSize 0
+  texture Texture2D $= Enabled
+  -- /Texture test
+  when (isJust texName) $ textureBinding Texture2D $= texName
+
   angle <- newIORef (0.0::GLfloat)
   env <- initSchemeEnv filename
   envRef <- newIORef env
