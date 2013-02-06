c73725f301bedfcd32750900231206663effe582
Hannes Gräuler <hgraeule@uos.de>
2013-02-06 23:21:40 +0100

    Also extract Rational type as a number

--- src/Flowskell/SchemeUtils.hs
+++ src/Flowskell/SchemeUtils.hs
@@ -4,5 +4,6 @@ import Data.Array
 
 extractFloat (Float n) = realToFrac n
 extractFloat (Number n) = realToFrac n
+extractFloat (Rational n) = realToFrac n
 
 makeFloatVector lst = Vector $ (listArray (0, (length lst) - 1)) (map Float lst)
