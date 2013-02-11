318ed33d07843fc3eaa3cd61f9f84285a4217351
Hannes Gräuler <hgraeule@uos.de>
2013-02-11 22:15:23 +0100

    Version bump, clean up

--- src/Flowskell/Lib/Textures.hs
+++ src/Flowskell/Lib/Textures.hs
@@ -1,39 +1,32 @@
 module Flowskell.Lib.Textures where
 import Graphics.Rendering.OpenGL hiding (Bool, Float)
-import Control.Monad
-import Control.Monad.Error
 import Graphics.Rendering.GLU.Raw
-import Graphics.Rendering.OpenGL.Raw.ARB.Compatibility (glPushMatrix, glPopMatrix)
 import Graphics.UI.GLUT hiding (Bool, Float)
-import Data.Time.Clock
-import Data.Time.Calendar
 import Data.Array
+import Data.IORef
 import Language.Scheme.Types
 
-import Flowskell.SchemeUtils
-import Flowskell.TextureUtils
-
-import Data.IORef
+import Flowskell.TextureUtils (getAndCreateTexture)
 
 loadTexture :: IORef [Maybe TextureObject] -> [LispVal] -> IO LispVal
-loadTexture textureListRef [String s] = do 
-                        textureList <- get textureListRef
+loadTexture txtLstRef [String s] = do 
+                        txtLst <- get txtLstRef
                         maybeTextureObj <- getAndCreateTexture s
-                        writeIORef textureListRef (textureList ++ [maybeTextureObj])
-                        return $ Number (fromIntegral (length textureList))
+                        writeIORef txtLstRef (txtLst ++ [maybeTextureObj])
+                        return $ Number (fromIntegral (length txtLst))
 
 setTexture :: IORef [Maybe TextureObject] -> [LispVal] -> IO LispVal
-setTexture textureListRef [Number n] = do
-                        textureList <- get textureListRef
-                        textureBinding Texture2D $= textureList !! (fromIntegral n)
+setTexture txtLstRef [Number n] = do
+                        txtLst <- get txtLstRef
+                        -- TODO: check bounds
+                        textureBinding Texture2D $= txtLst !! (fromIntegral n)
                         return (Number 1)
 
 initTextures :: IO [(String, [LispVal] -> IO LispVal)]
 initTextures = do
-    textureListRef <- newIORef []
-    print "textures initialised"
+    txtLstRef <- newIORef []
     return  [
-        ("load-texture", loadTexture textureListRef),
-        ("texture", setTexture textureListRef)
+        ("load-texture", loadTexture txtLstRef),
+        ("texture", setTexture txtLstRef)
         ]
 
