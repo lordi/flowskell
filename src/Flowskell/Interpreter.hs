55f3fd5b6f24bc2e9ffff40979395c330085239b
Hannes Gräuler <hgraeule@uos.de>
2013-03-07 23:53:50 +0100

    Some cleanup

--- src/Flowskell/Interpreter.hs
+++ src/Flowskell/Interpreter.hs
@@ -1,4 +1,4 @@
-module Flowskell.Interpreter where
+module Flowskell.Interpreter (initSchemeEnv, evalFrame) where
 import Control.Monad
 import Control.Monad.Error
 
@@ -22,23 +22,24 @@ mkPrimitiveList b = map (\(n, f) -> (("v", n), IOFunc $ makeThrowErrorFunc f)) b
                 where makeThrowErrorFunc f obj = liftIO $ f obj
 
 -- |Try to evaluate LispVal, call catch function if it fails
-try :: Env -> LispVal -> (LispError -> IO a) -> IO ()
-try env lisp catch =
+tryEval :: Env -> LispVal -> (LispError -> IO a) -> IO ()
+tryEval env lisp catch =
     evalLisp' env lisp >>= \x -> case x of
         Left error -> catch error >> return ()
         _ -> return ()
+
+reportError :: String -> LispError -> IO ()
+reportError file err = putStrLn $ "Failure in " ++ file ++ ":\n" ++ (show err)
 
 initSchemeEnv extraPrimitives filename = do
   let fskPrimitives = mkPrimitiveList $ defaultPrimitives ++ extraPrimitives
   fskLib <- getDataFileName "lib/flowskell.scm"
   env <- r5rsEnv >>= flip extendEnv fskPrimitives
-  mapM_ (\file -> do
-    try env (List [Atom "load", String file]) $ \ error -> do
-        putStrLn $ "Failure in " ++ file ++ ":\n" ++ (show error))
+  mapM_ (\f -> tryEval env (List [Atom "load", String f]) $ reportError f)
         [fskLib, filename]
   return env
 
-evalFrame env = do
-  try env (List [Atom "every-frame-entry-point"]) $ \ error -> do
-    print $ error
+evalFrame env =
+  tryEval env (List [Atom "every-frame-entry-point"]) $ \ error -> do
+    reportError "<source>" error
     evalString env "(define *has-error* #t)"
