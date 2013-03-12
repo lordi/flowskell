46653a41e4549c4dcffa5fb892ce13d83e2e4b86
Hannes Gräuler <graeuler@geoplex.de>
2013-03-12 17:34:38 +0100

    Improved parser slightly

--- src/Terminal/GetCharParser.hs
+++ src/Terminal/GetCharParser.hs
@@ -6,25 +6,30 @@ import Text.Parsec
 import Text.Parsec.String
 import Data.List (insert)
 
-data TerminalAction c = Newline | TChar c | ANSIAction deriving Show
+data TerminalAction c = Bell | Newline | TChar c | ANSIAction deriving Show
 
-playxxx :: String -> Either ParseError [TerminalAction Char]
-playxxx s = parse pxxx "parameter" s
+play :: String -> Either ParseError [TerminalAction Char]
+play s = parse pxxx "" s
 
 pxxx :: Parser [TerminalAction Char]
 pxxx = many (pchar <|> pansi)
+
 pansi = do
-    char 'E'
-    char ']'
+    string "\ESC]"
+    many digit
+    letter
     return ANSIAction
-pchar = (char '\n' >> return Newline) <|> (digit >>= return . TChar)
+pchar = (newline >> return Newline)
+    <|> (char '\a' >> return Bell)
+    <|> (satisfy (/= '\ESC') >>= return . TChar)
 
 stdinReader =
     forever $ do
         (liftIO getChar) >>= \x -> modify $ \y -> y ++ [x]
         s <- get
-        liftIO $ print (s, playxxx s)
+        liftIO $ print (s, play s)
 
 main = do
     hSetBuffering stdin NoBuffering
+    print $ play "awldjawlkdj1234\a\n\n\ESC]m\ESC]2K\n12\n"
     runStateT stdinReader ""
